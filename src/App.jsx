2eafc9256310758f488fcfb84ed9eee0416d12e6
Bruno Dantas <bruno96dantas@gmail.com>
2024-07-11 15:03:41 -0300

    estrutura da aplicacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-function App() {
+import { Header } from './components/Header';
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+export function App() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
+    </div>
   )
 }
-
-export default App
